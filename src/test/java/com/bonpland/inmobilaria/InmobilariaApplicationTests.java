3f261042958ec1769783531b173174f503b8dd55
BeadeYago <yagito2001@gmail.com>
2022-10-13 19:38:19 -0300

    start of the project

---
+++ src/test/java/com/bonpland/inmobilaria/InmobilariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonpland.inmobilaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmobilariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
