eeb07394c1ef9910f1105b67fa3a3a821b6f1a77
BeadeYago <yagito2001@gmail.com>
2022-10-13 19:52:00 -0300

    feat: add property service findAll

--- src/main/java/com/bonpland/inmobilaria/service/PropertyService.java
+++ src/main/java/com/bonpland/inmobilaria/service/PropertyService.java
@@ -1,4 +1,21 @@
 package com.bonpland.inmobilaria.service;
 
+import com.bonpland.inmobilaria.entity.Property;
+import com.bonpland.inmobilaria.repository.PropertyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PropertyService {
+
+    private PropertyService(PropertyRepository repo){
+        this.repo = repo;
+    }
+
+    private PropertyRepository repo;
+
+    public List<Property> findAll(){
+        return repo.findAll();
+    }
 }
