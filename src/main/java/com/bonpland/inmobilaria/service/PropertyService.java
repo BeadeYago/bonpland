8b9ec5a3ecdcff6f0d0867eeb445f93bfafed7f1
BeadeYago <yagito2001@gmail.com>
2022-10-20 21:16:13 -0300

    feat:"add method for delete, save, findbyid, edit"

--- src/main/java/com/bonpland/inmobilaria/service/PropertyService.java
+++ src/main/java/com/bonpland/inmobilaria/service/PropertyService.java
@@ -1,6 +1,7 @@
 package com.bonpland.inmobilaria.service;
 
 import com.bonpland.inmobilaria.entity.Property;
+import com.bonpland.inmobilaria.exception.EntityNotFoundException;
 import com.bonpland.inmobilaria.repository.PropertyRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +10,41 @@ import java.util.List;
 @Service
 public class PropertyService {
 
-    private PropertyService(PropertyRepository repo){
+    private PropertyService(PropertyRepository repo) {
         this.repo = repo;
     }
 
-    private PropertyRepository repo;
+    private final PropertyRepository repo;
 
-    public List<Property> findAll(){
+    public List<Property> findAll() {
         return repo.findAll();
+    }
+
+    public Property findById(Integer id) {
+        return repo.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Property with id: " + id + " not exist."));
+    }
+
+    public Property save(Property property){
+        return repo.save(property);
+    }
+
+    public void deleteById(Integer id) {
+        repo.deleteById(id);
+    }
+
+    public Property edit(Integer id, Property property) {
+
+        final Property propertyFromDB = repo.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Property with id: " + id + " not exist."));
+
+        propertyFromDB.setEnvironments(property.getEnvironments());
+        propertyFromDB.setCountry(property.getCountry());
+        propertyFromDB.setCity(property.getCity());
+        propertyFromDB.setPrice(property.getPrice());
+        propertyFromDB.setMeters(property.getMeters());
+        propertyFromDB.setType(property.getType());
+
+        return repo.save(propertyFromDB);
     }
 }
