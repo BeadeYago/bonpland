825c75c7bb7d158892ac6b7623a791508e5ca799
BeadeYago <yagito2001@gmail.com>
2022-10-20 21:13:57 -0300

    feat:"add annotation generated value"

--- src/main/java/com/bonpland/inmobilaria/entity/Property.java
+++ src/main/java/com/bonpland/inmobilaria/entity/Property.java
@@ -3,6 +3,8 @@ package com.bonpland.inmobilaria.entity;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
@@ -10,6 +12,7 @@ import javax.persistence.Id;
 public class Property {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private Integer environments;
@@ -23,5 +26,9 @@ public class Property {
     private Integer meters;
 
     private String type;
+
+    private String status;
+
+    private String image;
 
 }
