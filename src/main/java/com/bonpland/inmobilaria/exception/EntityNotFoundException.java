edb88aa51d08618e015674190c0ed7ddca9ec6da
BeadeYago <yagito2001@gmail.com>
2022-10-20 21:17:11 -0300

    feat: "add entitynotfound exception"

---
+++ src/main/java/com/bonpland/inmobilaria/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.bonpland.inmobilaria.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
